adceaab8729a0fef33991a25eb5c0a634b78a4db
mkpage1337 <mrku1337@gmail.com>
2024-07-15 19:53:32 +0200

    nav

---
+++ components/header.js
@@ -0,0 +1,17 @@
+export default function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a>Home</a>
+        </li>
+        <li>
+          <a>Page 1</a>
+        </li>
+        <li>
+          <a>Page 2</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
