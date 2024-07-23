ff218ba79debe9e8811886b475ae514384be0e1a
mkpage1337 <mrku1337@gmail.com>
2024-07-23 09:32:22 +0200

    adding more images

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,8 @@
 import NavBar from "@/components/NavBar";
 import Image from "next/image";
+import photo1 from "@/images/photo1.png";
+import photo2 from "@/images/photo2.png";
+import photo3 from "@/images/photo3.png";
 
 export default function Contact() {
   return (
@@ -7,17 +10,17 @@ export default function Contact() {
       <NavBar />
       <div>
         <div>
-          <Image src="" alt="" />
+          <Image src={photo1} alt="photo1" />
           <h3>TITLE</h3>
           <h4>TITLE</h4>
         </div>
         <div>
-          <Image src="" alt="" />
+          <Image src={photo2} alt="photo2" />
           <h3>TITLE</h3>
           <h4>TITLE</h4>
         </div>
         <div>
-          <Image src="" alt="" />
+          <Image src={photo3} alt="photo3" />
           <h3>TITLE</h3>
           <h4>TITLE</h4>
         </div>
