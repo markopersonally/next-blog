fb5b9583ffac9f49e0099dc50e67d4d0e8148ff9
mkpage1337 <mrku1337@gmail.com>
2024-07-24 11:37:25 +0200

    change

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,11 +1,11 @@
 import Link from "next/link";
 import NavBar from "@/components/NavBar";
 
-export default function BlogPage() {
+export default function PostsPage() {
   return (
     <main>
       <NavBar />
-      <h1>The Blog</h1>
+      <h1>Posts</h1>
       <p>
         <Link href="/blog/post-1">Post 1</Link>
       </p>
