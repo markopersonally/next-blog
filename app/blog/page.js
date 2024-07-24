ad2214270a41da6c3c029c9b3276dc47bb5318bc
mkpage1337 <mrku1337@gmail.com>
2024-07-24 11:09:36 +0200

    adding navbar to blog page

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import NavBar from "@/components/NavBar";
 
 export default function BlogPage() {
   return (
     <main>
+      <NavBar />
       <h1>The Blog</h1>
       <p>
         <Link href="/blog/post-1">Post 1</Link>
