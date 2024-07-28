962bc7f7434bfd54225f676b046bf65e5b022007
mkpage1337 <mrku1337@gmail.com>
2024-07-28 18:26:13 +0200

    change home page

--- components/section-post.js
+++ components/section-post.js
@@ -1,91 +1,21 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./section-post.module.css";
-import img1 from "@/images/cobra_1362523.png";
-import img2 from "@/images/elephant_1841047.png";
-import img3 from "@/images/frog_394697.png";
+import img1 from "@/images/boy-8530678_1280.png";
 
-export default function SectionPost() {
+const SectionPost = () => {
   return (
     <div id={styles["container"]}>
-      <Link href="/blog">
-        <div className={styles["wrapper-post"]}>
-          <Image
-            className={styles.picture}
-            src={img1}
-            alt="Picture of the author"
-          />
-          <div className={styles.description}>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </Link>
-      <Link href="/blog">
-        <div className={styles["wrapper-post"]}>
-          <Image
-            className={styles.picture}
-            src={img1}
-            alt="Picture of the author"
-          />
-          <div className={styles.description}>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </Link>
-      <Link href="/blog">
-        <div className={styles["wrapper-post"]}>
-          <Image
-            className={styles.picture}
-            src={img2}
-            alt="Picture of the author"
-          />
-          <div className={styles.description}>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </Link>
-      <Link href="/blog">
-        <div className={styles["wrapper-post"]}>
-          <Image
-            className={styles.picture}
-            src={img2}
-            alt="Picture of the author"
-          />
-          <div className={styles.description}>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </Link>
-      <Link href="/blog">
-        <div className={styles["wrapper-post"]}>
-          <Image
-            className={styles.picture}
-            src={img3}
-            alt="Picture of the author"
-          />
-          <div className={styles.description}>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </Link>
-      <Link href="/blog">
-        <div className={styles["wrapper-post"]}>
-          <Image
-            className={styles.picture}
-            src={img3}
-            alt="Picture of the author"
-          />
-          <div className={styles.description}>
-            <h3>Title</h3>
-            <p>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.</p>
-          </div>
-        </div>
-      </Link>
+      <div className={styles["main-post"]}>
+        <Link href="/" className={styles["main-post-link"]}>
+          <Image className={styles["main-post-image"]} src={img1} />
+          <h3 className={styles["main-post-title"]}>Main picture</h3>
+          <h4 className={styles["main-post-date"]}>28-07-2024</h4>
+        </Link>
+      </div>
+      <div className={styles["post-content"]}></div>
     </div>
   );
-}
+};
+
+export default SectionPost;
