a40203769013becba4b8d9a1e06b2c7a25ad8088
mkpage1337 <mrku1337@gmail.com>
2024-07-29 16:37:04 +0200

    change post

--- components/section-post.js
+++ components/section-post.js
@@ -6,13 +6,18 @@ import featuredPost from "@/data/HOME_DATA.tsx";
 const SectionPost = () => {
   return (
     <div id={styles["container"]}>
-      <div className={styles["main-post"]}>
-        <Link href="/" className={styles["main-post-link"]}>
-          <Image className={styles["main-post-image"]} src={featuredPost.img} />
-          <h3 className={styles["main-post-title"]}>{featuredPost.title}</h3>
-          <h4 className={styles["main-post-date"]}>{featuredPost.date}</h4>
-        </Link>
-      </div>
+      <Link href="/" className={styles["featured-post"]}>
+        <Image
+          className={styles["featured-post-image"]}
+          src={featuredPost.img}
+        />
+        <div className={styles["featured-post-description"]}>
+          <h3 className={styles["featured-post-title"]}>
+            {featuredPost.title}
+          </h3>
+          <h4 className={styles["featured-post-date"]}>{featuredPost.date}</h4>
+        </div>
+      </Link>
       <div className={styles["post-content"]}></div>
     </div>
   );
