509959651f5ccce7212805820b462ded38c61ac5
mkpage1337 <mrku1337@gmail.com>
2024-07-29 15:34:09 +0200

    create data to main post

--- components/section-post.js
+++ components/section-post.js
@@ -1,16 +1,16 @@
 import Link from "next/link";
 import Image from "next/image";
 import styles from "./section-post.module.css";
-import img1 from "@/images/boy-8530678_1280.png";
+import featuredPost from "@/data/HOME_DATA.tsx";
 
 const SectionPost = () => {
   return (
     <div id={styles["container"]}>
       <div className={styles["main-post"]}>
         <Link href="/" className={styles["main-post-link"]}>
-          <Image className={styles["main-post-image"]} src={img1} />
-          <h3 className={styles["main-post-title"]}>Main picture</h3>
-          <h4 className={styles["main-post-date"]}>28-07-2024</h4>
+          <Image className={styles["main-post-image"]} src={featuredPost.img} />
+          <h3 className={styles["main-post-title"]}>{featuredPost.title}</h3>
+          <h4 className={styles["main-post-date"]}>{featuredPost.date}</h4>
         </Link>
       </div>
       <div className={styles["post-content"]}></div>
