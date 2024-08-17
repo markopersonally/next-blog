86e1a9318e5d85bc287935cdd97c0aae7542624f
mkpage1337 <mrku1337@gmail.com>
2024-08-17 13:46:13 +0200

    adding id to data

--- data/HOME_DATA.tsx
+++ data/HOME_DATA.tsx
@@ -9,12 +9,14 @@ import univers from "@/images/univers.png";
 import cobra from "@/images/cobra_1362523.png";
 
 interface Post {
+  id: number;
   img: StaticImageData;
   title: string;
   date: string;
 }
 
 const featuredPost: Post = {
+  id: 1,
   img: featuredImg,
   title: "Exploring the Tourist Spots",
   date: "July 29, 2024",
@@ -22,26 +24,31 @@ const featuredPost: Post = {
 
 const postsList: Post[] = [
   {
+    id: 2,
     img: alien,
     title: "Alien Encounter",
     date: "August 1, 2024",
   },
   {
+    id: 3,
     img: cat,
     title: "Cats and Their Mysteries",
     date: "August 5, 2024",
   },
   {
+    id: 4,
     img: fish,
     title: "Underwater World",
     date: "August 10, 2024",
   },
   {
+    id: 5,
     img: univers,
     title: "Exploring the Universe",
     date: "September 29, 2024",
   },
   {
+    id: 6,
     img: cobra,
     title: "The World of Cobras",
     date: "October 15, 2024",
